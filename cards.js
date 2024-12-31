6d02d6b824b4d6b02dcec9e918563f7b3ec73698
Ahmet Esat KAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2024-12-31 20:51:14 +0300

    Add files via upload

--- cards.js
+++ cards.js
@@ -1,18 +1,20 @@
+var gamename = "KAREKÖK OYUNU";
+
 var cards = [
-    { word: '&#8730;20', meaning: '2&#8730;5' },
-    { word: '&#8730;60', meaning: '2&#8730;15' },
-    { word: '&#8730;24', meaning: '2&#8730;6' },
-    { word: '&#8730;180', meaning: '6&#8730;5' },
-    { word: '&#8730;80', meaning: '4&#8730;5' },
-    { word: '&#8730;98', meaning: '7&#8730;2' },
-    { word: '&#8730;125', meaning: '5&#8730;5' },
-    { word: '&#8730;50', meaning: '5&#8730;2' },
-    { word: '&#8730;72', meaning: '6&#8730;2' },
-    { word: '&#8730;8', meaning: '2&#8730;2' },
-    { word: '&#8730;27', meaning: '3&#8730;3' },
-    { word: '&#8730;200', meaning: '10&#8730;2' },
-    { word: '&#8730;48', meaning: '4&#8730;3' },
-    { word: '&#8730;45', meaning: '3&#8730;5' },
-    { word: '&#8730;162', meaning: '9&#8730;2' },
-    { word: '&#8730;243', meaning: '9&#8730;3' }
+    { word: '&#8730;20', meaning: '2&#8730;5', image: ''},
+    { word: '&#8730;60', meaning: '2&#8730;15', image: '' },
+    { word: '&#8730;24', meaning: '2&#8730;6', image: '' },
+    { word: '&#8730;180', meaning: '6&#8730;5', image: '' },
+    { word: '&#8730;80', meaning: '4&#8730;5', image: '' },
+    { word: '&#8730;98', meaning: '7&#8730;2', image: '' },
+    { word: '&#8730;125', meaning: '5&#8730;5', image: '' },
+    { word: '&#8730;50', meaning: '5&#8730;2', image: '' },
+    { word: '&#8730;72', meaning: '6&#8730;2', image: '' },
+    { word: '&#8730;8', meaning: '2&#8730;2', image: '' },
+    { word: '&#8730;27', meaning: '3&#8730;3', image: '' },
+    { word: '&#8730;200', meaning: '10&#8730;2', image: '' },
+    { word: '&#8730;48', meaning: '4&#8730;3', image: '' },
+    { word: '&#8730;45', meaning: '3&#8730;5', image: '' },
+    { word: '&#8730;162', meaning: '9&#8730;2', image: '' },
+    { word: '&#8730;243', meaning: '9&#8730;3', image: '' }
 ];
